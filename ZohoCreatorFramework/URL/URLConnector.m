d58f00a87ce69c202fd613268edb90bdc5ed7c5f
mohamedraffi <mohamed.raffi@zohocorp.com>
2014-01-24 10:37:10 +0530

    print log.

--- ZohoCreatorFramework/URL/URLConnector.m
+++ ZohoCreatorFramework/URL/URLConnector.m
@@ -92,7 +92,7 @@
             [postString setString:_paramString];
             NSString *postLength = [NSString stringWithFormat:@"%d",[postString length]];
             
-            //NSLog@"poststring %@",postString);
+            NSLog(@"poststring %@",postString);
 //            NSData * data=[postString dataUsingEncoding:NSUTF8StringEncoding];
 //            NSString * stringafterdecoding = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
             
